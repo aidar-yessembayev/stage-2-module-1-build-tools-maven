19ced357d8dede5b705f95824c14c8a843300989
Aidar Yessembayev <yessembayevaidar@gmail.com>
2023-02-12 22:40:25 +0600

    init

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,21 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if (str == null) {
+            return false;
+        }
+
+        try {
+            double number = Double.parseDouble(str);
+
+            if (number > 0) {
+                return true;
+            }
+
+            return false;
+
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 }
